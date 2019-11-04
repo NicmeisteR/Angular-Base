eef89a7d0fd90dab50734e9731d605a33c52386e
Nicolaas Nel <nicon@infoslips.com>
2019-11-04 10:04:07 +0200

    Initial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// ██╗   ██╗██╗███████╗██╗    ██╗███████╗
+// ██║   ██║██║██╔════╝██║    ██║██╔════╝
+// ██║   ██║██║█████╗  ██║ █╗ ██║███████╗
+// ╚██╗ ██╔╝██║██╔══╝  ██║███╗██║╚════██║
+//  ╚████╔╝ ██║███████╗╚███╔███╔╝███████║
+//   ╚═══╝  ╚═╝╚══════╝ ╚══╝╚══╝ ╚══════╝
+//                                       
+import { HomeComponent } from './views/home/home.component';
+import { AboutComponent } from './views/about/about.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'about', component: AboutComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash:true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
